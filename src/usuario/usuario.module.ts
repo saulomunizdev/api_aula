4d04567c836f48ba4955507939fbe140cc0a8259
Saulo Muniz <saulomuniz.dev@gmail.com>
2025-09-26 21:44:52 -0300

    alterado o usuario module

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { UsuarioController } from "./usuario.controller";
 import { UsuariosArmazenados } from "./usuario.dm";
 import { EmailUnicoValidator } from "./validacao/email-unico.validator";
+import { StrongPassValidator } from "./validacao/strong-pass.validator";
 
 @Module({
     controllers: [UsuarioController],
-    providers: [UsuariosArmazenados, EmailUnicoValidator],
+    providers: [UsuariosArmazenados, EmailUnicoValidator, StrongPassValidator],
 })
 export class UsuarioModule {}
