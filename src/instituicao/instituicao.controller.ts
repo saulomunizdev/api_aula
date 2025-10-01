a9d6d389668689cb37f5cb233f41f337e32a2a1a
Saulo Muniz <saulomuniz.dev@gmail.com>
2025-09-30 21:08:04 -0300

    Colocado mensagem de erros

--- src/instituicao/instituicao.controller.ts
+++ src/instituicao/instituicao.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Post, Get, Put, Delete, Param} from "@nestjs/common";
+import {Body, Controller, Post, Get, Put, Delete, Param, NotFoundException, InternalServerErrorException} from "@nestjs/common";
 import { instituicaoEntity } from "./instituicao.entity";
 import {v4 as uuid} from 'uuid';
 import { CriaInstituicaoDTO } from "./dto/criarInstituicao.dto";
@@ -7,6 +7,7 @@ import { ListaInstituicaoDTO } from "./dto/listaInstituicao.dto";
 import { AlteraInstituicaoDTO } from "./dto/alteraInstituicao.dto";
 import { ApiTags } from "@nestjs/swagger";
 import { LoginDTO } from "./dto/login.dto";
+import { RetornoPadraoDTO } from "src/dto/retorno.dto";
 
 @Controller('/instituicao')
 @ApiTags('instituicao')
@@ -16,52 +17,83 @@ export class InstituicaoController {
     }
 
     @Put('/:id')
-        async atualizaInstituicao(@Param('id') id: string, @Body() dadosAtualizacao: AlteraInstituicaoDTO){
-        const instituicaoAtualizado = await this.Instituicao.atualizaInstituicao(id, dadosAtualizacao);
-        return {
-        evento: instituicaoAtualizado,
-        message: 'Instituição atualizado com sucesso!'
-        };
+        async atualizaInstituicao(@Param('id') id: string, @Body() dadosAtualizacao: AlteraInstituicaoDTO): Promise <RetornoPadraoDTO>{
+        try{
+            const instituicaoAtualizado = await this.Instituicao.atualizaInstituicao(id, dadosAtualizacao);
+            return new RetornoPadraoDTO(
+            'Instituição atualizado com sucesso!',
+            instituicaoAtualizado,
+            );
+        }catch (error) {
+            if (error.message === 'Usuário não encontrado'){
+                throw new NotFoundException(`Usuário com id $(id) não encontrado`);
+            }
+                throw new InternalServerErrorException('Erro inesperado no servidor');
+        }    
     }
 
     @Delete('/:id')
-        async deletaInstituicao(@Param('id') id: string){
-        const instituicaoRemovido = await this.Instituicao.removeInstituicao(id);
-        return {
-        evento: instituicaoRemovido,
-        message: 'Instituição removido com sucesso!'
-        };
+        async deletaInstituicao(@Param('id') id: string): Promise <RetornoPadraoDTO>{
+            try{
+                const instituicaoRemovido = await this.Instituicao.removeInstituicao(id);
+                return new RetornoPadraoDTO(
+                'Instituição removido com sucesso!',
+                instituicaoRemovido
+                );
+            } catch (error) {
+                if (error.message === 'Usuário não encontrado'){
+                throw new NotFoundException(`Usuário com id $(id) não encontrado`);
+            }
+                throw new InternalServerErrorException('Erro inesperado no servidor');
+            }
     }
 
     @Post('/login')
-        async login(@Body() dadosLogin: LoginDTO) {
-        const usuarioLogado = this.Instituicao.loginUsuario(dadosLogin.email, dadosLogin.senha);   
-        if(usuarioLogado){
-            return {
-                usuario: usuarioLogado,
-                message: 'Login realizado com sucesso'
-            };
-        }
-        return {
-            message: 'Email ou senha inválidos'
-        };
+        async login(@Body() dadosLogin: LoginDTO): Promise <RetornoPadraoDTO> {
+            try{
+                const usuarioLogado = this.Instituicao.loginUsuario(dadosLogin.email, dadosLogin.senha);   
+                if(usuarioLogado){
+                    return new RetornoPadraoDTO(
+                        'Login realizado com sucesso',
+                        usuarioLogado
+                    );
+                }
+                    return  new RetornoPadraoDTO(
+                    'Email ou senha inválidos',
+                    null
+                 );
+            } catch (error) {
+                if (error.message === 'Usuário não encontrado'){
+                throw new NotFoundException(`Usuário com id $(id) não encontrado`);
+            }
+                throw new InternalServerErrorException('Erro inesperado no servidor');
+            }    
     }  
 
 
 
 
     @Post()
-    async criaInstituicao(@Body() dadosInstituicao: CriaInstituicaoDTO) {
-        var novoInstituicao = new instituicaoEntity(uuid(),dadosInstituicao.nomeInstituicao, dadosInstituicao.nomeResponsavel, dadosInstituicao.cidade, dadosInstituicao.estado,
-                                                    dadosInstituicao.telefone, dadosInstituicao.email, dadosInstituicao.senha);
+    async criaInstituicao(@Body() dadosInstituicao: CriaInstituicaoDTO): Promise <RetornoPadraoDTO> {
+        try{
+            var novoInstituicao = new instituicaoEntity(uuid(),dadosInstituicao.nomeInstituicao, dadosInstituicao.nomeResponsavel, dadosInstituicao.cidade, dadosInstituicao.estado,
+                                                        dadosInstituicao.telefone, dadosInstituicao.email, dadosInstituicao.senha);
 
-        this.Instituicao.AdicionarInstituicao(novoInstituicao);
-        var retorno = { 
-            novoInstituicao,
-            message: 'Instituição criado com sucesso'
-        };
-        return retorno;
+            this.Instituicao.AdicionarInstituicao(novoInstituicao);
+            var retorno = new RetornoPadraoDTO( 
+                'Instituição criado com sucesso',
+                novoInstituicao
+            );
+            return retorno;
+       } catch (error) {
+                if (error.message === 'Usuário não encontrado'){
+                throw new NotFoundException(`Usuário com id $(id) não encontrado`);
+            }
+                throw new InternalServerErrorException('Erro inesperado no servidor');
+            } 
     }
+
+
 
 
      @Get()
--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Body, Controller, Delete, Get, InternalServerErrorException, NotFoundException, Param, Post, Put } from "@nestjs/common";
 import { UsuarioEntity } from "./usuario.entity";
 import { UsuariosArmazenados } from "./usuario.dm";
 import {v4 as uuid} from 'uuid';
@@ -7,6 +7,7 @@ import { ListaUsuarioDTO } from "./dto/listaUsuario.dto";
 import { ApiTags } from "@nestjs/swagger";
 import { AlteraUsuarioDTO } from "./dto/AlteraUsuarios.dto";
 import { LoginUsuariosDTO } from "./dto/loginUsuarios.dto";
+import { RetornoPadraoDTO } from "src/dto/retorno.dto";
 
 @Controller('/usuarios')
 @ApiTags('usuario')
@@ -16,19 +17,24 @@ export class UsuarioController {
     }
 
     @Post()
-    async criaUsuario(@Body() dadosUsuario: CriaUsuarioDTO) {
+    async criaUsuario(@Body() dadosUsuario: CriaUsuarioDTO): Promise <RetornoPadraoDTO> {
+        try{
         var novoUsuario = new UsuarioEntity(uuid(),dadosUsuario.nome, dadosUsuario.idade, 
                                             dadosUsuario.cidade, dadosUsuario.email, 
                                             dadosUsuario.telefone, dadosUsuario.senha);
 
-      
-        
         this.Usuarios.AdicionarUsuario(novoUsuario);
-        var retorno = { 
-            novoUsuario,
-            message: 'Usuário criado com sucesso'
-        };
+        var retorno = new RetornoPadraoDTO( 
+            'Usuário criado com sucesso',
+            novoUsuario
+        );
         return retorno;
+        }catch (error) {
+             if (error.message === 'Usuário não encontrado'){
+                        throw new NotFoundException(`Usuário com id $(id) não encontrado`);
+                    }
+                        throw new InternalServerErrorException('Erro inesperado no servidor');
+        }
     }
 
     @Get()
@@ -47,36 +53,59 @@ export class UsuarioController {
 
 
      @Put('/:id')
-            async atualizaUsuarios(@Param('id') id: string, @Body() dadosAtualizacao: AlteraUsuarioDTO){
-            const UsuariosAtualizado = await this.Usuarios.atualizaUsuarios(id, dadosAtualizacao);
-            return {
-            evento: UsuariosAtualizado,
-            message: 'Instituição atualizado com sucesso!'
-            };
+            async atualizaUsuarios(@Param('id') id: string, @Body() dadosAtualizacao: AlteraUsuarioDTO):Promise <RetornoPadraoDTO>{
+                try{
+                    const UsuariosAtualizado = await this.Usuarios.atualizaUsuarios(id, dadosAtualizacao);
+                    return new RetornoPadraoDTO(
+                        'Usuário atualizado com sucesso!',
+                        UsuariosAtualizado 
+                    );
+                }catch(error){
+                    if (error.message === 'Usuário não encontrado'){
+                throw new NotFoundException(`Usuário com id $(id) não encontrado`);
+                }
+                throw new InternalServerErrorException('Erro inesperado no servidor');
+                }    
+             
         }
     
         @Delete('/:id')
-            async deletaInstituicao(@Param('id') id: string){
-            const UsuariosRemovido = await this.Usuarios.removeUsuarios(id);
-            return {
-            evento: UsuariosRemovido,
-            message: 'Instituição removido com sucesso!'
-            };
+            async deletaUsuario(@Param('id') id: string): Promise <RetornoPadraoDTO>{
+                try{
+                    const UsuariosRemovido = await this.Usuarios.removeUsuarios(id);
+                    return new RetornoPadraoDTO(
+                'Usuário removido com sucesso!',
+                UsuariosRemovido
+                    );
+                    } catch (error) {
+                if (error.message === 'Usuário não encontrado'){
+                throw new NotFoundException(`Usuário com id $(id) não encontrado`);
+            }
+                throw new InternalServerErrorException('Erro inesperado no servidor');
+            }
         }
     
         @Post('/login')
-            async login(@Body() dadosLogin: LoginUsuariosDTO) {
-            const usuarioLogado = this.Usuarios.loginUsuario(dadosLogin.email, dadosLogin.senha);   
-            if(usuarioLogado){
-                return {
-                    usuario: usuarioLogado,
-                    message: 'Login realizado com sucesso'
-                };
-            }
-            return {
-                message: 'Email ou senha inválidos'
-            };
-        }  
+            async login(@Body() dadosLogin: LoginUsuariosDTO): Promise <RetornoPadraoDTO> {
+                try{
+                    const usuarioLogado = this.Usuarios.loginUsuario(dadosLogin.email, dadosLogin.senha);   
+                    if(usuarioLogado){
+                    return new RetornoPadraoDTO(
+                        'Login realizado com sucesso',
+                        usuarioLogado
+                    );
+                }
+                    return  new RetornoPadraoDTO(
+                    'Email ou senha inválidos',
+                    null
+                 );
+            } catch (error) {
+                if (error.message === 'Usuário não encontrado'){
+                throw new NotFoundException(`Usuário com id $(id) não encontrado`);
+                }
+                throw new InternalServerErrorException('Erro inesperado no servidor');
+            }    
+        }        
 
 
 
